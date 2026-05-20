7e69a7039f4ebd38c75cc0321da859f2193fd872
Nish <nishpatha09@gmail.com>
2026-05-20 16:50:06 -0700

    added other folders

---
+++ Week_1_C/arrays/arrays.c
@@ -0,0 +1,9 @@
+//compile: gcc arrays.c -o arrays
+//run: ./arrays
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
---
+++ Week_1_C/pointers/pointers.c
@@ -0,0 +1,9 @@
+//compile: gcc pointers.c -o pointers
+//run: ./pointers
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
