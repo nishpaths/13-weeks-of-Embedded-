42a3be91d51caf959cba11df2292e54214a1c07b
Nish <nishpatha09@gmail.com>
2026-06-10 12:26:55 -0700

    Working on V2 of REMU

--- Week_2_Advanced_Embedded_C/register_emulator/main.c
+++ Week_2_Advanced_Embedded_C/register_emulator/main.c
@@ -16,53 +16,82 @@ typedef enum {
     SYSTEM_READY = 7
 } RegisterBit;
 
+typedef enum{
+    READ_WRITE = 0,
+    READ_ONLY = 1,
+    WRITE_ONLY = 2,
+}ACCESS_TYPE;
 
-void set_bit(uint8_t *reg, RegisterBit bit);
-void clear_bit(uint8_t *reg, RegisterBit bit);
-uint8_t read_bit(uint8_t reg, RegisterBit bit);
-void write_mask(uint8_t *reg, uint8_t mask);
-void print_reg(uint8_t reg);
+typedef struct{
+    uint8_t value;
+    ACCESS_TYPE access_type;
+    char name[50];
+}Register;
 
-void set_bit(uint8_t *reg, RegisterBit bit){
+typedef struct{
+    Register PORTA;
+    Register DDRB;
+    Register STATUS;
+    Register CMD;
+}MMIO_System;
+
+
+void read();
+void write(uint8_t value, Register *reg);
+
+void set_bit(Register *reg, RegisterBit bit);
+void clear_bit(Register *reg, RegisterBit bit);
+uint8_t read_bit(Register *reg, RegisterBit bit);
+void write_mask(Register*reg, uint8_t mask);
+void print_reg(Register *reg);
+
+void set_bit(Register *reg, RegisterBit bit){
 
     if (bit < LED_ENABLE || bit > SYSTEM_READY){
         printf("Invalid bit number\n");
         return;
     }
-    *reg |= (1 << bit);
+    reg->value |= (1 << bit);
 }
-void clear_bit(uint8_t *reg, RegisterBit bit){
+void clear_bit(Register *reg, RegisterBit bit){
     if (bit < LED_ENABLE || bit > SYSTEM_READY){
         printf("Invalid bit number\n");
         return;
     }
-    *reg &= ~(1 << bit);
+    reg->value &= ~(1 << bit);
 }
-uint8_t read_bit(uint8_t reg, RegisterBit bit){
+uint8_t read_bit(Register *reg, RegisterBit bit){
     if (bit < LED_ENABLE || bit > SYSTEM_READY){
         printf("Invalid bit number\n");
         return 0;
     }
-    return (reg & (1 << bit)) ? 1 : 0;
+    return (reg->value & (1 << bit)) ? 1 : 0;
 }
-void write_mask(uint8_t *reg, uint8_t mask){
-    *reg = mask;
+void write_mask(Register *reg, uint8_t mask){
+    reg->value = mask;
 }
-void print_reg(uint8_t reg){
+void print_reg(Register *reg){
     printf("Register: 0b");
 
     for(int i = 7; i >= 0; i--){
-        printf("%d", (reg >> i) & 1);
+        printf("%d", (reg->value >> i) & 1);
     }
 
     printf("\n");
 }
 
 int main(){
-    uint8_t REG = 0b10101010;
-    printf("MOTOR_ENABLE: %d\n", read_bit(REG, MOTOR_ENABLE));
-    printf("LED_ENABLE: %d\n", read_bit(REG, LED_ENABLE));
-    printf("SYSTEM_READY: %d\n", read_bit(REG, SYSTEM_READY));
+    MMIO_System mcu ={
+        .PORTA = {0x00, READ_WRITE, "PORTA"},
+        .DDRB = {0x00, READ_WRITE, "DDRB"},
+        .STATUS = {0x00, READ_ONLY, "STATUS"},
+        .CMD = {0x00, WRITE_ONLY, "CMD"},
+    };
+    print_reg(&mcu.PORTA);
+    print_reg(&mcu.DDRB);
+    print_reg(&mcu.STATUS);
+    print_reg(&mcu.CMD);
+    return 0;
 }
 
 
